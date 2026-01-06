e9f9c66f10a1ffe94794349e198d7e69e1c26ba6
MrBinit <sapkotabinit2002@gmail.com>
2026-01-06 12:57:57 +0545

    added ./requirements.txt and as it was causing some instalation issue in the deployment

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from httpx import AsyncClient
 from app.main import app
 
-# async lets the test client run inside the same async event loop as FastAPI.
 @pytest.fixture
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
+
